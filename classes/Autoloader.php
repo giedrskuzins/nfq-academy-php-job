0aacaae433c8737a53c4ca8f268271a8dcacb812
user <user>
2017-03-21 03:00:57 +0200

    Finished autoload

--- classes/Autoloader.php
+++ classes/Autoloader.php
@@ -1,10 +1,16 @@
 <?php
-require_once("classes/autoload/Psr4AutoloaderClass.php");
-require_once("classes/autoload/Psr4AutoloaderExt.php");
+
 class Autoloader {
 	
     static public function loader($className) {
-		$classNameOrig=$className;
+
+
+        self::loaderSimple($className);
+
+    }
+
+    static public function loaderSimple($className) {
+
         $className=str_replace("App","classes",$className);
 
         $filename = "./" . str_replace('\\', '/', $className) . ".php";
@@ -14,48 +20,10 @@ class Autoloader {
             if (class_exists($className)) {
                 return TRUE;
             }
-        }else{
-			self::loaderPsr4($classNameOrig);
-		}
+        }
+
         return FALSE;
+
     }
-	
-	static public function loaderPsr4($className) {
-		//protected $loader;
-        //$this->loader = new Psr4AutoloaderExt;
-		
-        $loader = new Psr4AutoloaderExt;
-
-        //$this->loader->setFiles(array(
-		$loader->setFiles(array(
-            //'/vendor/foo.bar/src/ClassName.php',
-            '/classes/Animal.php',
-            '/classes/Ball.php',
-            '/classes/Cloneable.php',
-            '/classes/Person.php',
-            '/classes/Shared.php',
-			'/classes/Vehicle.php',
-			'/classes/test/TestDebugInfo.php',
-			'/classes/test/other/TestSetState.php',
-			'/classes/test/other/TestSetState.php',
-        ));
-
-        $loader->addNamespace(
-            //'Foo\Bar',
-			'App',
-            //'/vendor/foo.bar/src'
-			'/classes'
-        );
-		
-		$loader->addNamespace(
-            //'Foo\Bar',
-			'Vendor',
-            //'/vendor/foo.bar/src'
-			'/Vendor'
-        );
-		
-		//$actual = $this->loader->loadClass('Foo\Bar\ClassName');
-		$actual = $loader->loadClass($className);
-	}
 }
 spl_autoload_register('Autoloader::loader');
